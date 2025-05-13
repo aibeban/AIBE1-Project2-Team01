9f51521bcb62409363b49629a59c6aa5a9861617
aibeban <aibeban@github.com>
2025-05-13 14:31:09 +0900

    first commit

---
+++ gradle/src/main/java/org/sunday/projectpop/ProjectPopApplication.java
@@ -0,0 +1,16 @@
+package org.sunday.projectpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+
+
+@SpringBootApplication
+public class ProjectPopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPopApplication.class, args);
+    }
+
+}
